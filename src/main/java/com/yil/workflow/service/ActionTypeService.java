51341272fbd5858286257f96c530fbdf4ccc1098
yasin.yildirim <yasin.yildirim>
2022-06-25 15:39:35 +0300

    add transactional

--- src/main/java/com/yil/workflow/service/ActionTypeService.java
+++ src/main/java/com/yil/workflow/service/ActionTypeService.java
@@ -12,8 +12,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class ActionTypeService {
 
