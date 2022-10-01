669a1563415cfdf15f0057c928cd1cd8f4cf8984
yasin.yildirim <yasin.yildirim>
2022-10-01 14:32:25 +0300

    jwt token düzenlendi. tüm servisler token üzerinden çalışması sağlandı

--- src/main/java/com/yil/workflow/base/ApiConstant.java
+++ src/main/java/com/yil/workflow/base/ApiConstant.java
@@ -2,7 +2,7 @@ package com.yil.workflow.base;
 
 public class ApiConstant {
 
-    public static final String AUTHENTICATED_USER_ID = "Authenticated-UserId";
+    public static final String AUTHENTICATED_USER_ID = "Authenticated-User-Id";
     public static final String PAGE_SIZE = "1000";
     public static final String PAGE = "0";
 }
